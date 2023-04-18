4e02eb3ddb64b2b7ff0dd93ffbf5187ade067830
AliyevMura <7lvghl9@code.edu.az>
2023-04-18 10:21:05 +0400

    initial commit

---
+++ Models/Galery.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17april.Models
+{
+    internal class Galery
+    {
+
+        public Car[] cars = new Car[0];
+        public int Id;
+        public Galery()
+        {
+
+        }
+        public Galery(int id)
+        {
+            Id = id;
+        }
+        public void AddCar(Car car)
+        {
+            Array.Resize(ref cars, cars.Length + 1);
+            cars[cars.Length - 1] = car;
+        }
+        public void ShowAllCars()
+        {
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car.Name);
+                Console.WriteLine(car.Color);
+                Console.WriteLine(car.Speed);
+                
+            }
+        }
+        public Car[] GetAllCars()
+        {
+            
+            return cars;
+        }
+        public void FindById()
+        {
+            foreach (var car in cars)
+            {
+                if (this.Id == car.Id)
+                {
+                    Console.WriteLine(car.Name);
+                }
+            }
+        }
+       
+
+    }
+}
